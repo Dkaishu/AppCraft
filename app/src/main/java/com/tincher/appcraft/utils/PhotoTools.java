83f1e86150c55f5c5834865d2fc0c7930b316d1c
dks <dongkaishu999@gmail.com>
2017-11-24 13:30:06 +0800

    launchMode测试

--- app/src/main/java/com/tincher/appcraft/utils/PhotoTools.java
+++ app/src/main/java/com/tincher/appcraft/utils/PhotoTools.java
@@ -97,6 +97,39 @@ public class PhotoTools {
     }
 
     /**
+     * 调用相机拍照
+     */
+    public static void takePhoto(Activity activity, File takeImageFile, Intent takePictureIntent, int requestCode) {
+        if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+            if (takeImageFile != null) {
+                // 默认情况下，即不需要指定intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                // 照相机有自己默认的存储路径，拍摄的照片将返回一个缩略图。如果想访问原始图片，
+                // 可以通过data extra能够得到原始图片位置。即，如果指定了目标uri，data就没有数据，
+                // 如果没有指定uri，则data就返回有数据！
+                Uri uri;
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+                    uri = Uri.fromFile(takeImageFile);
+                } else {
+                    /**
+                     * 7.0 调用系统相机拍照不再允许使用Uri方式，应该替换为FileProvider
+                     * 并且这样可以解决MIUI系统上拍照返回size为0的情况
+                     */
+                    uri = FileProvider.getUriForFile(activity, getFileProviderName(activity), takeImageFile);
+                    //加入uri权限 要不三星手机不能拍照
+                    List<ResolveInfo> resInfoList = activity.getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                    for (ResolveInfo resolveInfo : resInfoList) {
+                        String packageName = resolveInfo.activityInfo.packageName;
+                        activity.grantUriPermission(packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    }
+                }
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            }
+        }
+        activity.startActivityForResult(takePictureIntent, requestCode);
+    }
+
+    /**
+     * 与manifest文件一致的唯一id
      * 用于解决provider冲突
      */
     private static String getFileProviderName(Context context) {

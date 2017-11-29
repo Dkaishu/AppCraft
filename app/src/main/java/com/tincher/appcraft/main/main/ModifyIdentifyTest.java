6d0b00d7d31117fd74f1bcb5b7f5a1aab4f6c3f9
dks <dongkaishu999@gmail.com>
2017-11-29 14:03:38 +0800

    ArcGIS测试

---
+++ app/src/main/java/com/tincher/appcraft/main/main/ModifyIdentifyTest.java
@@ -0,0 +1,19 @@
+package com.tincher.appcraft.main.main;
+
+import com.tincher.appcraft.base.BaseActivity;
+
+/**
+ * Created by dks on 2017/11/29.
+ */
+
+public class ModifyIdentifyTest extends BaseActivity {
+    @Override
+    protected int initLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
